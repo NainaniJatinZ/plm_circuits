8ed7e563f1746ac3e31fa297dbb16b45de8f4f07
NainaniJatinZ <nainani.jatin.0@gmail.com>
2025-08-12 03:21:37 +0000

    chore: remove obsolete files and scripts related to edge attribution and circuit diagrams

--- notebooks/circuit_diagram.py
+++
@@ -1,8 +0,0 @@
-"""
-
-1. load the layer latent dicts for l4, 8, 12 
-2. find all edges between those and one where they are involved 
-3. extrapolate old plotting code to account for 3 layers :w
-
-
-"""
